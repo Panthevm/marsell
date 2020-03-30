108f5643891351fbfc6ea36e7f8d59408235a70a
panthevm <panthevm@yandex.ru>
2020-03-30 22:05:31 +0300

    Remove build file

--- client/src/app/build/prod.cljs
+++
@@ -1,4 +0,0 @@
-(ns app.dev
-  (:require [app.core      :as core]))
-
-(core/mount)
--- client/src/app/components/header/core.cljs
+++ client/src/app/components/header/core.cljs
@@ -5,14 +5,14 @@
 (defn component []
   [:header.container.m-between.border-bottom.center
    [:button.m-none.btn {:on-click #(rf/dispatch [::h/expands :navbar])}
-    [:img.icon.pointer {:src "../icons/menu.svg"}]]
+    [:img.icon.pointer {:src "icons/menu.svg"}]]
    [:h1 "MARSELL"]
    [:button.m-none.btn
-    [:img.icon.pointer {:src "../icons/basket.svg"}]]
+    [:img.icon.pointer {:src "icons/basket.svg"}]]
    [:div.none.m-block
     [:div.row
-     [:img.icon.pointer.pr {:src "../icons/phone.svg"}]
-     [:img.icon.pointer.pr {:src "../icons/basket.svg"}]
+     [:img.icon.pointer.pr {:src "icons/phone.svg"}]
+     [:img.icon.pointer.pr {:src "icons/basket.svg"}]
      [:div.row.center.pointer
       [:div.space
        [:span.muted "0 шт. - "] [:b "0.00 "]]
