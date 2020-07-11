77cd0f54180d99981fe17f5ab7354acaf97c774e
panthevm <panthevm@yandex.ru>
2020-07-11 17:00:00 +0300

    Re-frame to hash-map

--- client/src/app/core.cljs
+++ client/src/app/core.cljs
@@ -4,8 +4,9 @@
 
             ;Frames
             [frames.xhr]
-            [frames.page    :as page]
-            [frames.routing :as routing]
+            [frames.page           :as page]
+            [frames.routing        :as routing]
+            [frames.re-frame-patch :as rfp]
 
             ;App
             [app.routes :as routes]
@@ -21,9 +22,9 @@
   {:base-url   "http://localhost:8080/"
    :client-url "http://localhost:3000/"})
 
-(rf/reg-event-fx
+(rfp/reg-event-fx
  ::initialize
- (fn [{db :db} _]
+ (fn [{db :db}]
    {:db (assoc db :config config)
     :frames.routing/init routes/routes}))
 
@@ -33,14 +34,27 @@
     [:div "Страница не найдена"]))
 
 (defn current-page []
-  (let [route (rf/subscribe [::routing/current])]
+  (let [route (rfp/subscribe {:id ::routing/current})]
     (fn []
       (let [page   (->> @route :match (get @page/pages))
             params (->> @route :params)]
         [:<>
+         [:button {:on-click #(rfp/dispatch {:id ::db :params :reg-event-db})} "event-db"]
+         [:button {:on-click #(rfp/dispatch {:id ::fx})}                       "event-fx"]
          [navbar/component]
          [content page params]]))))
 
 (defn ^:export mount []
-  (rf/dispatch-sync [::initialize])
+  (rfp/dispatch-sync {:id ::initialize})
   (dom/render [current-page] (js/document.getElementById "app")))
+
+(do ;test
+  (rfp/reg-event-db
+   ::db
+   (fn [{:keys [db params]}]
+     (assoc db :type params)))
+
+  (rfp/reg-event-fx
+   ::fx
+   (fn [{:keys [params]}]
+     {::rfp/dispatch {:id ::db :params :reg-event-fx}})))
