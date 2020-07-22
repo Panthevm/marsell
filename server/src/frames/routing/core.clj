12aa1cf4175ab235b70538744dac7684eec31d4e
panthevm <panthevm@yandex.ru>
2020-07-22 23:05:27 +0300

    Add server routing params parse

--- server/src/frames/routing/core.clj
+++ server/src/frames/routing/core.clj
@@ -1,22 +1,29 @@
 (ns frames.routing.core)
 
 (defn match*
-  [node [current & other :as path]]
-  (cond (get node current)               (recur (get node current) other)
-        (and (seq path) (get node :var)) (recur (:var node) other)
-        :else                            node))
+  [current-node [current-path & other :as path] params]
+  (letfn [(get-params-name [node]
+            (ffirst (filter (comp symbol? key) node)))
+          (set-params-node [params-name]
+            (assoc params (keyword  params-name) current-path))]
+    (let [next-node   (get current-node current-path)
+          params-name (get-params-name current-node)]
+      (cond
+        next-node                    (recur next-node                      other params)
+        (and (seq path) params-name) (recur (get current-node params-name) other (set-params-node params-name))
+        :else                        [current-node params]))))
 
 (defn match
   [router uri]
-  (match* router (re-seq #"[^/]+" uri)))
+  (match* router (re-seq #"[^/]+" uri) {}))
 
 (defn response
-  [request resource options]
+  [request [resource params] options]
   (let [method  (:method request)
         handler (-> resource method :handler)]
     (cond
       (= :OPTIONS method) resource
-      (fn? handler)       (handler request)
+      (fn? handler)       (handler (assoc request :params params))
       (not handler)       (:not-found options))))
 
 (defn routing
--- server/test/frames/routing/core_test.clj
+++ server/test/frames/routing/core_test.clj
@@ -3,26 +3,31 @@
             [matcho.core         :as matcho]
             [clojure.test        :refer [deftest testing]]))
 
-(def resource
-  {:GET    {:handler (fn [request] {:status 200 :body (:method request)})}
-   :POST   {:handler (fn [request] {:status 201 :body (:method request)})}
-   :DELETE {:handler (fn [request] {:status 200 :body (:method request)})}
-   "child" {:GET     {:handler (fn [request] {:status 200 :body "child"})}}
-   :var    {:GET     {:handler (fn [request] {:status 200 :body "var-get"})}
-            "nested" {:GET {:handler (fn [request] {:status 200 :body "nested"})}}}})
-
 (def routing
   (sut/routing
-   {"resource" resource}
+   {"resource" {:GET    {:handler (fn [request] (:method request))}
+                :POST   {:handler (fn [request] (:method request))}
+                :DELETE {:handler (fn [request] (:method request))}
+                "child" {:GET     {:handler (fn [request] "child")}}
+                'var    {:GET     {:handler (fn [request] (:params request))}
+                         "nested" {:GET {:handler (fn [request] "nested")}
+                                   'id  {:GET {:handler (fn [request] (:params request))}}}}}}
    {:not-found {:status 404 :body {:message "Resource not found"}}}))
 
 (deftest routing-core-test
 
   (testing "methods"
-    (matcho/match (routing {:uri "/resource" :method :GET})     {:status 200 :body :GET})
-    (matcho/match (routing {:uri "/resource" :method :POST})    {:status 201 :body :POST})
-    (matcho/match (routing {:uri "/resource" :method :DELETE})  {:status 200 :body :DELETE})
-    (matcho/match (routing {:uri "/resource" :method :OPTIONS}) resource))
+    (matcho/match (routing {:uri "/resource" :method :GET})     :GET)
+    (matcho/match (routing {:uri "/resource" :method :POST})    :POST)
+    (matcho/match (routing {:uri "/resource" :method :DELETE})  :DELETE)
+    (matcho/match (routing {:uri "/resource" :method :OPTIONS})
+                  {:GET    {:handler fn?}
+                   :POST   {:handler fn?}
+                   :DELETE {:handler fn?}
+                   "child" {:GET     {:handler fn?}}
+                   'var    {:GET     {:handler fn?}
+                            "nested" {:GET {:handler fn?}
+                                      'id  {:GET {:handler fn?}}}}}))
 
   (testing "options"
     (testing "not-found"
@@ -30,6 +35,11 @@
                     {:status 404 :body {:message "Resource not found"}})))
 
   (testing "params"
-    (matcho/match (routing {:uri "/resource/child"    :method :GET}) {:status 200 :body "child"})
-    (matcho/match (routing {:uri "/resource/1"        :method :GET}) {:status 200 :body "var-get"})
-    (matcho/match (routing {:uri "/resource/1/nested" :method :GET}) {:status 200 :body "nested"})))
+    (matcho/match (routing {:uri "/resource/child"    :method :GET})   "child")
+    (matcho/match (routing {:uri "/resource/1"        :method :GET})   {:var "1"})
+    (matcho/match (routing {:uri "/resource/1/nested" :method :GET})   "nested")
+    (matcho/match (routing {:uri "/resource/1/nested/2" :method :GET}) {:var "1" :id "2"}))
+
+  (testing "not-found"
+    (matcho/match (routing {:uri "/foo"    :method :GET}) {:status 404 :body {:message "Resource not found"}})
+    (matcho/match (routing {:uri "/foo/bar":method :GET}) {:status 404 :body {:message "Resource not found"}})))
