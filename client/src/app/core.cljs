7c99bfcc70baa466765ada8c5b5564ef725bfdb3
panthevm <panthevm@yandex.ru>
2020-08-25 00:01:19 +0300

    [Client] Remove UI

--- client/src/app/core.cljs
+++ client/src/app/core.cljs
@@ -6,42 +6,28 @@
             [frames.xhr]
             [frames.page           :as page]
             [frames.routing        :as routing]
-            [frames.re-frame-patch :as rfp]
 
             ;App
-            [app.routes :as routes]
-
-            ;Pages
-            [app.pages.home.core]
-            [app.pages.auth.core]
-
-            ;Components
-            [app.components.navbar.core  :as navbar]))
+            [app.routes :as routes]))
 
 (def ^:const config
   {:base-url   "http://localhost:8080/"
    :client-url "http://localhost:3000/"})
 
-(rfp/reg-event-fx
+(rf/reg-event-fx
  ::initialize
- (fn [{:keys [db]}]
+ (fn [{db :db} _]
    {:db                  (assoc db :config config)
     :frames.routing/init routes/routes}))
 
-(defn content [page params]
-  (if page
-    [page params]
-    [:div "Страница не найдена"]))
-
 (defn current-page []
-  (let [route (rfp/subscribe {:id ::routing/current})]
+  (let [route (rf/subscribe [::routing/current])]
     (fn []
       (let [page   (->> @route :match (get @page/pages))
             params (->> @route :params)]
         [:<>
-         [navbar/component]
-         [content page params]]))))
+         [:div "Hello"]]))))
 
 (defn ^:export mount []
-  (rfp/dispatch-sync {:id ::initialize})
+  (rf/dispatch-sync [::initialize])
   (dom/render [current-page] (js/document.getElementById "app")))
