d8fdf1e5e083852c064d7d50d18f893401bc4983
panthevm <panthevm@yandex.ru>
2020-09-07 21:26:53 +0300

    [Client] Add form frames

---
+++ client/src/frames/form.cljc
@@ -0,0 +1,89 @@
+(ns frames.form
+  "https://github.com/HealthSamurai/zenform"
+  (:refer-clojure :exclude [eval])
+  (:require [re-frame.core :as reframe]))
+
+(defn init
+  [schema value]
+  (cond
+    (= :form (:type schema))
+    (update schema :value
+            (partial reduce-kv
+                     (fn [accumulator node-name node-schema]
+                       (assoc accumulator node-name
+                              (init node-schema (get value node-name))))
+                     {}))
+
+    (and (= :collection (:type schema))
+         value)
+    (assoc schema
+           :value
+           (reduce-kv
+            (fn [accumulator index item]
+              (assoc accumulator index
+                     (init (:value schema) item)))
+            {} value)
+
+           :schema (:value schema))
+
+    :else
+    (assoc schema :value value)))
+
+(defn eval
+  [node]
+  (cond
+    (= :form (:type node))
+    (reduce-kv
+     (fn [accumulator node-name node-schema]
+       (assoc accumulator node-name
+              (eval node-schema)))
+     {} (:value node))
+
+    (and (= :collection (:type node))
+         (not-empty (:value node)))
+    (reduce-kv
+     (fn [accumulator index node-schema]
+       (conj accumulator (eval node-schema)))
+     [] (:value node))
+
+    :else (:value node)))
+
+(defn node-path
+  [form-path path]
+  (vec (concat form-path
+               (->> path (interpose :value) (cons :value)))))
+
+(reframe/reg-event-db
+ ::init
+ (fn [db [_ {:keys [params]}]]
+   (assoc-in db (:form-path params)
+             (init (:form-schema params) (:data params)))))
+
+(reframe/reg-event-db
+ ::add-collection-item
+ (letfn [(next-index [coll]
+           (->> coll keys (apply max) inc))]
+   (fn [db [_ {:keys [params]}]]
+     (update-in db (node-path (:form-path params) (:path params))
+                (fn [node]
+                  (assoc-in node [:value (next-index (:value node))]
+                            (init (:schema node) (:value params))))))))
+
+(reframe/reg-event-db
+ ::remove-collection-item
+ (fn [db [_ {:keys [params]}]]
+   (update-in db (conj (node-path (:form-path params) (:path params))
+                       :value)
+              dissoc (:index params))))
+
+(reframe/reg-event-db
+ ::set-value
+ (fn [db [_ {:keys [params]}]]
+   (assoc-in db (conj (node-path (:form-path params) (:path params))
+                      :value)
+             (:value params))))
+
+(reframe/reg-sub
+ ::form
+ (fn [db [_ {:keys [params]}]]
+   (get-in db (:form-path params))))
---
+++ client/test/frames/form_test.clj
@@ -0,0 +1,96 @@
+(ns frames.form-test
+  (:require [frames.form  :as sut]
+
+            [re-frame.core :as reframe]
+            [re-frame.db   :as db]
+
+            [matcho.core  :as matcho]
+            [clojure.test :refer [deftest testing]]))
+
+(def schema
+  {:form-path   [:form ::schema1]
+   :form-schema {:type  :form
+                 :value {:birth-date {:type :string}
+                         :period     {:type  :form
+                                      :value {:start {:type :string}}}
+                         :name       {:type  :collection
+                                      :value {:type  :form
+                                              :value {:given  {:type :collection}
+                                                      :family {:type :string}}}}
+                         :address    {:type  :collection
+                                      :value {:type  :form
+                                              :value {:text {:type :string}}}}}}
+   :data        {:birth-date "1970-01-01"
+                 :period     {:start "1999-09-09"}
+                 :name       [{:given ["Name"  "Surname"]  :family "Family"}
+                              {:given ["Name2" "Surname2"] :family "Family2"}]
+                 :address    [{:text "address-1"}
+                              {:text "address-2"}]}})
+
+
+(deftest model
+  (reset! db/app-db nil)
+  (testing "events"
+    (testing "eval"
+      (testing "with data"
+        (reframe/dispatch [::sut/init {:params schema}])
+        (matcho/match
+         (sut/eval @(reframe/subscribe [::sut/form {:params schema}]))
+         (:data schema)))
+      (testing "without data"
+        (reframe/dispatch [::sut/init {:params (dissoc schema :data)}])
+        (matcho/match
+         (sut/eval @(reframe/subscribe [::sut/form {:params schema}]))
+         {:name nil :address nil :period {:start nil}})))
+
+    (testing "set-value"
+      (reframe/dispatch [::sut/init {:params schema}])
+      (reframe/dispatch
+       [::sut/set-value {:params (assoc schema
+                                        :path  [:period :start]
+                                        :value "2000-09-09")}])
+      (reframe/dispatch
+       [::sut/set-value {:params (assoc schema
+                                        :path  [:birth-date]
+                                        :value "1970-01-01")}])
+      (reframe/dispatch
+       [::sut/set-value {:params (assoc schema
+                                        :path  [:name 0 :family]
+                                        :value "family-2")}])
+      (matcho/match
+       (sut/eval @(reframe/subscribe [::sut/form {:params schema}]) )
+       (-> (:data schema)
+           (assoc-in [:period :start]  "2000-09-09")
+           (assoc-in [:name 0 :family] "family-2")
+           (assoc-in [:birth-date]     "1970-01-01"))))
+
+    (testing "collection"
+      (testing "add"
+        (reframe/dispatch [::sut/init {:params schema}])
+
+        (reframe/dispatch
+         [::sut/add-collection-item {:params (assoc schema
+                                                    :path  [:name]
+                                                    :value {:family "family3"
+                                                            :given  ["name3" "surname3"]})}])
+
+        (reframe/dispatch
+         [::sut/add-collection-item {:params (assoc schema
+                                                    :path  [:name 2 :given]
+                                                    :value "surname-added")}])
+        (matcho/match
+         (sut/eval @(reframe/subscribe [::sut/form {:params schema}]))
+         (-> (:data schema)
+             (update :name conj {:family "family3"
+                                 :given  ["name3" "surname3" "surname-added"]}))))
+      (testing "remove"
+        (reframe/dispatch [::sut/init {:params schema}])
+
+        (reframe/dispatch
+         [::sut/remove-collection-item {:params (assoc schema
+                                                       :path  [:name]
+                                                       :index 0)}])
+        (matcho/match
+         (sut/eval @(reframe/subscribe [::sut/form {:params schema}]))
+         (-> (:data schema)
+             (update :name rest)))))))
